79f47e7b45a285bce16be272155a29569578526c
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-01-03 17:00:03 +0900

    [Bronze V] Title: A+B - 7, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -0,0 +1,10 @@
+a = int(input())
+list = []
+
+for i in range(a):
+    num1,num2 = map(int, input().split())
+    list.append(num1+num2)
+
+
+for i in range(a):
+    print(f'Case #{i+1}: {list[i]}')
