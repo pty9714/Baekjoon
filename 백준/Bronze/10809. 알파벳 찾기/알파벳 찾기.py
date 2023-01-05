c5c22e10f542ad142a206df02a0e36445e766783
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-01-06 06:03:21 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 124 ms, Memory: 34800 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,15 @@
+from string import ascii_lowercase
+dict = {} 
+for i in ascii_lowercase:
+    dict[i] = -1 # dict에 {'a':-1, 'b':-1 ..., 'z':-1} 입력
+
+a = list(input())
+count = 0 # 몇번째에 등장하는지 세는 자리수
+for i in a:
+    if dict[i] == -1:
+        dict[i] = count # dict에 -1 대신 자리수를 입력
+        count +=1
+    else:
+        count +=1 # 중복된 알파벳이 있는경우 입력하지않고 다음 자리
+for i in dict:
+    print(dict[i],end=' ')
