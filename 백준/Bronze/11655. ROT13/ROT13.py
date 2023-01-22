8b412855efbb5c97f3785921910c0ca5125ab0b6
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-01-22 14:25:50 +0900

    [Bronze I] Title: ROT13, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,10 @@
+s = input()
+upp = [chr(x) for x in range(ord('A'),ord('Z')+1)]
+low = [chr(x) for x in range(ord('a'),ord('z')+1)]
+for i in s:
+    if i in upp:
+        print(chr((ord(i)-ord('A')+13)%26+ord('A')),end='')
+    elif i in low:
+        print(chr((ord(i)-ord('a')+13)%26+ord('a')),end='')
+    else:
+        print(i,end='')
