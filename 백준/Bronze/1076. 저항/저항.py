2e209788b415fb94be26e09e596b55e8f6a752f2
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-01-25 10:04:40 +0900

    [Bronze II] Title: 저항, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,6 @@
+dic = {'black' : 0, 'brown' : 1, 'red':2,'orange':3,'yellow':4,'green':5,'blue':6,'violet':7,'grey':8,'white':9}
+a = input()
+b = input()
+c = input()
+
+print(int(str(dic[a]) + str(dic[b]))*(10**dic[c]))
