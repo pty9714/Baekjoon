5ca1de39314ec24d876e2fd77717acbafd73dd4e
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-01-26 13:38:13 +0900

    [Silver V] Title: 카드1, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,8 +1,6 @@
+#2161
 l = [x for x in range(1,int(input())+1)]
 while len(l) !=1:
-    print(l[0],end=' ')
-    l.pop(0)
-    x = l[0]
-    l.pop(0)
-    l.append(x)
+    print(l.pop(0),end=' ')
+    l.append(l.pop(0))
 print(l[0])
