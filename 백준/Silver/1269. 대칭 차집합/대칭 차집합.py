ac0146dbe611faedcf69d0d4734e500855b7087c
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-01-27 12:59:56 +0900

    [Silver IV] Title: 대칭 차집합, Time: 228 ms, Memory: 98720 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -1,19 +1,6 @@
-a,b = map(int,input().split())
-l1 = list(map(int,input().split()))
+import sys
+input = sys.stdin.readline            
+a,b  = map(int,input().split())
+l = list(map(int,input().split()))
 l2 = list(map(int,input().split()))
-k = {}
-for i in l1:
-    if i not in k:
-        k[i] = 1
-    else:
-        k[i] +=1
-for i in l2:
-    if i not in k:
-        k[i] = 1
-    else:
-        k[i] +=1
-count = 0
-for j in k:
-    if k[j]==1:
-        count +=1
-print (count)
+print(len(set(l)^set(l2)))
