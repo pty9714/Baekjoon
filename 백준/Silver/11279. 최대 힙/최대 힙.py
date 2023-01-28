27d7a4ca1ce46cf2de0cfbf94e396b07ae629814
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-01-28 12:54:50 +0900

    [Silver II] Title: 최대 힙, Time: 176 ms, Memory: 36984 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,12 @@
+import sys
+# sys.stdin = open('input.txt','r')
+input= sys.stdin.readline
+import heapq
+l = []
+for _ in range(int(input())):
+    a = int(input())
+    if a != 0:
+        heapq.heappush(l,-a)
+    else:
+        try: print(heapq.heappop(l)*-1)
+        except: print(0)
