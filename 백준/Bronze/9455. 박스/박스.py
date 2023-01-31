ce007e71067bfb4c88657bf6d39b276170d6285c
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-01-31 15:03:46 +0900

    [Bronze I] Title: 박스, Time: 128 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9455. 박스/박스.py
+++ 백준/Bronze/9455. 박스/박스.py
@@ -4,19 +4,13 @@ tc = int(input())
 for _ in range(tc):
     a,b = map(int,input().split())
     g = []
-    cnt = -1
-    total = 0
     for _ in range(a):
         g.append(list(map(int,input().split())))
-    while cnt != 0:
-        cnt = 0
-        for i in range(a-1,0,-1):
-            for j in range(b-1,-1,-1):
-                if g[i][j] == 0 and g[i-1][j] == 1:
-                    g[i][j] = 1
-                    g[i-1][j] = 0
-                    cnt+=1
-
-        total +=cnt
-    
+    total = 0
+    for i in range(b):
+        box = 0
+        for j in range(a-1,-1,-1):
+            if g[j][i] == 1:
+                total += a-1-j-box
+                box +=1   
     print(total)
