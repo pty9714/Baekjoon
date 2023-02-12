fb535ec1eec76849e1f50405eeee4aed2aebd57e
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-02-12 20:35:52 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 592 ms, Memory: 58584 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def bfs(a):
+    global cnt
+    stack = deque([a])
+    visited[a] = 1
+    while stack:
+        cur = stack.popleft()
+        for i in l[cur]:
+            if visited[i] == 0:
+                stack.append(i)
+                cnt +=1
+                visited[i] = cnt
+                
+n,m,r = map(int,input().split())
+cnt = 1
+l = [[] for _ in range(n+1)]
+visited = [0]*(n+1)
+for _ in range(m):
+    a,b = map(int,input().split())
+    l[a].append(b)
+    l[b].append(a)  
+for i in range(n+1):
+    l[i] = sorted(l[i])
+
+bfs(r)
+for j in range(1,n+1):
+    print(visited[j])
+# bfs 데크
