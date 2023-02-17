a72dde444ce39c3ae62e036440ed69ab49385f98
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-02-17 21:34:00 +0900

    [Silver II] Title: 마인크래프트, Time: 804 ms, Memory: 117424 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,33 +1,28 @@
 import sys
+input = sys.stdin.readline
+n,m,b = map(int,input().split())
+g = []
 
-n, m, b = map(int, sys.stdin.readline().split())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-answer = sys.maxsize
-idx = 0
+for _ in range(n):
+    l = list(map(int,input().split()))
+    g.append(l)
 
-# 0층부터 256층까지 반복
-for target in range(257):
-    max_target, min_target = 0, 0
-
-    # 반복문을 통해 블록을 확인
-    for i in range(n):
-        for j in range(m):
-
-            # 블록이 층수보다 더 크면
-            if graph[i][j] >= target:
-                max_target += graph[i][j] - target
-
-            # 블록이 층수보다 더 작으면
+ans_time = 500*500*256
+ans_height = 0
+for i in range(257):
+    plus_block = 0
+    minus_block = 0
+    for j in range(n):
+        for k in range(m):
+            if g[j][k] > i:
+                plus_block += g[j][k] -i
             else:
-                min_target += target - graph[i][j]
-
-    # 블록을 뺀 것과 원래 있던 블록의 합과 블록을 더한 값을 비교
-    # 블록을 뺀 것과 원래 있던 블록의 합이 더 커야 층을 만들 수 있음.
-    if max_target + b >= min_target:
-        # 시간 초를 구하고 최저 시간과 비교 
-        if min_target + (max_target * 2) <= answer:
-        	# 0부터 256층까지 비교하므로 업데이트 될수록 고층의 최저시간
-            answer = min_target + (max_target * 2) # 최저 시간
-            idx = target # 층수
-
-print(answer, idx)
+                minus_block +=i-g[j][k]
+    
+    if b+plus_block-minus_block>=0:
+        if ans_time>=plus_block*2+minus_block:
+            ans_time = plus_block*2+minus_block
+            ans_height = i
+        else:
+            break
+print(ans_time,ans_height)
