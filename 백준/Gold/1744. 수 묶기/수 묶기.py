35c3e0d5fb3a5bc67226a8c3e9465d3331490b5e
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-02-18 20:49:46 +0900

    [Gold IV] Title: 수 묶기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -0,0 +1,49 @@
+num = int(input())
+total = 0
+minusl = []
+plusl = []
+zero_check = False
+for i in range(num):
+    a = int(input())
+    if a>0:
+        plusl.append(a)
+    elif a<0:
+        minusl.append(a)
+    else:
+        zero_check=True
+
+plusl.sort(reverse=True)
+minusl.sort()
+if len(plusl)%2==0:
+    for i in range(len(plusl)//2):
+        a= plusl[2*i]
+        b= plusl[2*i+1]
+        if a == 1 or b == 1:
+            total +=a+b
+        else:
+            total +=a*b
+else:
+    for i in range(len(plusl)//2):
+        a= plusl[2*i]
+        b= plusl[2*i+1]
+        if a == 1 or b == 1:
+            total +=a+b
+        else:
+            total +=a*b
+    total += plusl[-1]
+
+if len(minusl)%2==0:
+    for i in range(len(minusl)//2):
+        a= minusl[2*i]
+        b= minusl[2*i+1]
+        total +=a*b
+else:
+    for i in range(len(minusl)//2):
+        a= minusl[2*i]
+        b= minusl[2*i+1]
+        total +=a*b
+    if zero_check == False:
+        total += minusl[-1]
+
+
+print(total)
