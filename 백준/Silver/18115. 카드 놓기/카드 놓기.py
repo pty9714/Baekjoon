4763c0c9d7e257e4c490ff3f3f9c274bb0093d27
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-02-25 01:30:42 +0900

    [Silver III] Title: 카드 놓기, Time: 844 ms, Memory: 86200 KB -BaekjoonHub

--- 백준/Silver/18115. 카드 놓기/카드 놓기.py
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.py
@@ -1,26 +1,22 @@
 from collections import deque
-a = int(input())
-l = list(map(int,input().split()))
-numl = [i for i in range(1,a+1)]
-ans = []
-deq = deque([])
-ans2 = deque([])
-a = len(l)
-for i in range(a):
-    if l[i] == 1:
-        if deq:
-            deq.appendleft(numl.pop())
-            ans +=deq
-            deq.clear()
-        else:
-            ans.append(numl.pop())
-    elif l[i] == 2:
-        a = numl.pop()
-        deq.append(a)
-    else:
-        ans2.appendleft(numl.pop())
 
-print(*(ans),end=' ')
-if deq:
-    print(*(deq),end=' ')
-print(*(ans2),end=' ')
+n = int(input())
+order = list(map(int, input().split()))
+result = deque()
+
+for i in range(1, n + 1):
+    # 뒤로가기
+    now = order.pop()
+    # 기술 1: 가장 위에 있는 것을 카드 뭉치 가장 위에 옮긴다
+    if now == 1:
+        result.appendleft(i)
+    # 기술 2: 가장 위에 있는 것을 카드 뭉치의 위에서 두 번째 자리로 옮긴다
+    elif now == 2:
+        temp = result.popleft()
+        result.appendleft(i)
+        result.appendleft(temp)
+    # 기술 3: 가장 위에 있는 것을 카드 뭉치 가장 아래로 옮긴다
+    elif now == 3:
+        result.append(i)
+
+print(*result)
