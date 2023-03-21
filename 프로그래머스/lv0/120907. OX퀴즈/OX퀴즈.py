a07cbbc99c1da62b83b630f5aa51d743de19548a
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-03-22 08:56:13 +0900

    [level 0] Title: OX퀴즈, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,17 @@
+def solution(quiz):
+    answer = []
+    for i in quiz:
+        x,operation,y,equal,z = i.split()
+        if operation == '+':
+            if int(x)+int(y) == int(z):
+                answer.append('O')
+            else:
+                answer.append('X')
+        else:
+            if int(x)-int(y) == int(z):
+                answer.append('O')
+            else:
+                answer.append('X')   
+
+    
+    return answer
