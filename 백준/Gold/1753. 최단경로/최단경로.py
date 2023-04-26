1059067feb7b8b5df0f17262beebc579775bc22e
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-04-27 00:41:38 +0900

    [Gold IV] Title: 최단경로, Time: 704 ms, Memory: 68688 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -18,8 +18,6 @@ def dijkstra(start):
     distance[start] = 0
     while q:
         dist, now = heapq.heappop(q)
-        if distance[now]< dist:
-            continue
         for i in graph[now]:
             cost = dist + i[1]
             if cost < distance[i[0]]:
