27bfc035274fd4efe3231b09abebb3935f3ce15b
pty9714 <121421631+pty9714@users.noreply.github.com>
2023-07-04 10:25:20 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,10 +1,13 @@
-from itertools import combinations
-l = []
-for _ in range(9):
-    l.append(int(input()))
+list = []
+for i in range(9):
+    list.append(int(input()))
 
-k = combinations(l,7)
-for i in k:
-    if sum(i) == 100:
-        print(*sorted(i),sep='\n')
-        break
+list.sort()
+
+for i in range(9):
+    for j in range(i+1,9):
+        if sum(list)-list[i]-list[j] == 100:
+            for k in range(9):
+                if k != i and k != j:
+                    print(list[k])
+            exit()
