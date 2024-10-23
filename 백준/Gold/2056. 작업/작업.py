74f1eca47079ce4b256772ceb03b73ada0932b28
pty9714 <121421631+pty9714@users.noreply.github.com>
2024-10-23 23:07:15 +0900

    [Gold IV] Title: 작업, Time: 568 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2056. 작업/작업.py
@@ -0,0 +1,8 @@
+n = int(input())
+dp = [0]*(n+1)
+for i in range(1, n+1):
+    work, count, *pre = map(int, input().split())
+    dp[i] = work
+    for j in pre:
+        dp[i] = max(dp[i], dp[j]+work)
+print(max(dp))
