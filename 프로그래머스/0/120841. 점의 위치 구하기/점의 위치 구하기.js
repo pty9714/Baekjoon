41fd091b0d923be1fdd114430e92bcd89510cdab
pty9714 <121421631+pty9714@users.noreply.github.com>
2025-04-29 08:43:04 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,16 @@
+function solution(dot) {
+    var answer = 0;
+    if (dot[0]>0 && dot[1]>0){
+        answer = 1;
+    }
+    else if (dot[0]>0 && dot[1]<0){
+        answer = 4;
+    }
+    else if (dot[0]<0 && dot[1]<0){
+        answer = 3;
+    }
+    else{
+        answer = 2;
+    }
+    return answer;
+}
