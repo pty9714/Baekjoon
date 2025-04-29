2ba5b085c9d085fa05cf1c1dba426c26d34b6f01
pty9714 <121421631+pty9714@users.noreply.github.com>
2025-04-29 09:18:53 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,13 @@
+function solution(price) {
+    var answer = price;
+    if (price>=500000){
+        answer = parseInt(price*0.8)
+    }
+    else if (price>=300000){
+        answer = parseInt(price*0.9)
+    }
+    else if (price>=100000){
+        answer = parseInt(price*0.95)
+    }
+    return answer;
+}
