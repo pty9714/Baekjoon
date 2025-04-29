5297d05dda668f24ddc3e63cb1240a7fde318b9d
pty9714 <121421631+pty9714@users.noreply.github.com>
2025-04-29 18:56:12 +0900

    [level 0] Title: 합성수 찾기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = 0;
+    for (let i=1; i<=n; i++){
+        for (let j=2; j<i; j++){
+            if (i%j==0){
+                answer+=1;
+                break;
+            }
+        }
+    }
+    return answer;
+}
